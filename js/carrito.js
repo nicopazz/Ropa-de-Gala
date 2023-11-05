9b96973da5db9bdc2b8a9d7633e6d0e507a223b0
Mauricio Torres <mauriciotorres781@gmail.com>
2023-11-05 11:43:17 -0300

    los cambios realizados fueron para agregarr el footer a las diferentes páginas

--- js/carrito.js
+++ js/carrito.js
@@ -1,5 +1,8 @@
 import navbar from '../componentes/navbar.js';
+import footer from '../componentes/footer.js';
+
 document.addEventListener('DOMContentLoaded', navbar);
+document.addEventListener('DOMContentLoaded', footer);
 
 
 
--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
 import productos from './arrayProductos.js';
 import navbar from '../componentes/navbar.js';
+import footer from '../componentes/footer.js';
 
 document.addEventListener('DOMContentLoaded', navbar);
+document.addEventListener('DOMContentLoaded', footer);
+
 
 
 const contenedorCards = document.querySelector('.contenedorCards');
