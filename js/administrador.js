e6c0535a3af182b96b9b8ed631e062ebc937ebb7
Nicolas <nicopazmalizia@gmail.com>
2023-11-07 12:01:08 -0300

    Se agrega hoja de estilos pagina admin y se solucionan problemas de botones en pag admin js eliminando module de script en html

--- js/administrador.js
+++ js/administrador.js
@@ -1,28 +1,6 @@
 
+
 const arrNuevosProductos = JSON.parse(localStorage.getItem("Productos"));
-
-
-arrNuevosProductos.forEach(producto => {
-  const productRow = document.createElement('tr');
-  productRow.classList.add('rowTable');
-  let imputCheck = '';
-  
-  productRow.innerHTML = `
-  <td class="text-center ">${producto.titulo}</td>
-  <td class="text-center">${producto.categoria} </td>
-  <td class="text-center">${producto.precio} </td>
-  <td class="text-center">${producto.stock} </td>
-  <td><img src = "${producto.imagen}" style= "width:5rem"></td>
-  <td class="text-center"><input class="inputCheck" type="checkbox" ></td>
-  <td class="text-center"> 
-    <button type="button" class="btn btn-info" id="editButton" >Editar</button>
-    <button type="button" class="btn btn-danger" id="deleteProducto" >Borrar</button>
-  </td>
-  `
-  
-  document.querySelector('tbody').appendChild(productRow);
-  
-});
 
 
 const datosIngresados = (e) => {
@@ -33,8 +11,13 @@ const datosIngresados = (e) => {
     const precio = document.getElementById("precio").value;
     const stock = document.getElementById("stock").value;
     const imagen = document.getElementById("imagen").value;
-  
-    const nuevoProducto = {titulo,categoria,precio,stock,imagen}
+    
+    let lastCode = 0;
+    
+    if(localStorage.getItem('Productos')){
+      lastCode = ((JSON.parse(localStorage.getItem('Productos'))).pop()).id
+    }
+    const nuevoProducto = {titulo,categoria,precio,stock,imagen,id:parseInt(lastCode) + 1}
 
   if (titulo && categoria && precio && stock && imagen) {
     arrNuevosProductos.push(nuevoProducto);
@@ -51,6 +34,53 @@ const datosIngresados = (e) => {
     
 
 }
+
+
+const check = (e) => {
+  e.preventDefault();
+  console.log(e);
+}
+
+function borrar(id){
+  
+  console.log(id);
+  
+ 
+}
+
+const editarProducto = (id) => {
+ 
+  console.log(id);
+}
+
+
+    arrNuevosProductos.forEach(producto => {
+      const productRow = document.createElement('tr');
+      productRow.classList.add('rowTable');
+      let inputCheck = '';
+    if(producto.publicado){
+     inputCheck = 'checked'
+      }
+      productRow.innerHTML = `
+      <td class="text-center ">${producto.titulo}</td>
+      <td class="text-center">${producto.categoria} </td>
+      <td class="text-center">${producto.precio} </td>
+      <td class="text-center">${producto.stock} </td>
+      <td><img src = "${producto.imagen}" style= "width:5rem"></td>
+      <td class="text-center"><input ${inputCheck} class="inputCheck" type="checkbox" id="check" ></td>
+      <td class="text-center" > 
+        <button type="button" class="btn btn-info" onclick="editarProducto('${producto.id}')" >Editar</button>
+        <button type="button" class="btn btn-danger" onclick="borrar('${producto.id}')" >Borrar</button>
+      </td>
+      `
+      
+      document.querySelector('tbody').appendChild(productRow);
+  
+    });
+
+
+
+
 
 function clear() {
   document.getElementById("nuevoProducto").value = "";
@@ -92,23 +122,10 @@ function mostrarNuevosProductos() {
 }
 */
 
-const borrarProducto = () => {
-  
-  alert('click');
-  
- 
-}
-
-const editarProducto = () => {
-  
-  alert('click');
-  
- 
-}
 
 
-const botonEditar = document.getElementById("editButton").addEventListener("click",editarProducto);
-const botonBorrar = document.getElementById("deleteProducto").addEventListener("click",borrarProducto);
+
+
 const button = document.getElementById("formData").addEventListener("submit",datosIngresados);
-//mostrarNuevosProductos();
+
 
